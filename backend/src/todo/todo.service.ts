ad047a8de0bdd3095e0355a077a30f4eb1eb81fb
Udhaykrishn <krishnauday320@gmail.com>
2024-06-20 19:35:36 +0530

    added repository for todo and change the todo.services and added there create function

--- backend/src/todo/todo.service.ts
+++ backend/src/todo/todo.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { TodoRepository } from './repository/todo.repository';
+import { todoDto } from './dto';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+    constructor(private readonly repository:TodoRepository){
+    }
+
+    create(data:todoDto){
+        return this.repository.create(data)
+    }
+}
