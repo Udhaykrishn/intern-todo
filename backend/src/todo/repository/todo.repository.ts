efd2728c9113944261325af4f1638f4cab4ebee5
Udhaykrishn <krishnauday320@gmail.com>
2024-06-21 16:34:05 +0530

    added create in repo

--- backend/src/todo/repository/todo.repository.ts
+++ backend/src/todo/repository/todo.repository.ts
@@ -1,5 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from '../../prisma/prisma.service';
+import { todoDto } from "../dto";
 
 export class TodoRepository{
-   
+    constructor(private readonly prsima:PrismaService){}
+
+    create(data:todoDto){
+        return this.prsima.todo.create({
+            data:data
+        })
+    }
 }
