3d4d12d2fceae2dce9fde7a8e23d8470a664aacc
Udhaykrishn <krishnauday320@gmail.com>
2024-06-21 16:56:18 +0530

    added the todo repository

--- backend/src/todo/todo.module.ts
+++ backend/src/todo/todo.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { TodoRepository } from './repository/todo.repository';
 
 @Module({
   controllers: [TodoController],
-  providers: [TodoService]
+  providers: [TodoService,TodoRepository]
 })
 export class TodoModule {}
